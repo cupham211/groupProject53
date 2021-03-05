12c0391eeaa488cade062320205381d69bae139d
Andrew Yemtsev <yemtseva@flip3.engr.oregonstate.edu>
2021-03-04 23:09:38 -0800

    Finished Function 3 and added test cases for it

---
+++ task.py
@@ -0,0 +1,261 @@
+# CS 362-400 Group 53
+
+# Function 1 Author: Christine Pham
+def conv_num(num_str):
+    """function that takes a string of numbers and converts it to int/float.
+    conv_num processes the string and calls the appropriate converting func."""
+    negSign, charPos, perCount = 1, -1, 0
+
+    # check if parameter is a string
+    if not isinstance(num_str, str):
+        return None
+
+    # check if a value is being passed
+    if len(num_str) == 0:
+        return None
+
+    # check for negative sign & remove
+    if num_str == '-':
+        return None
+    elif num_str[0] == '-':
+        negSign = -1
+        num_str = num_str[1:]
+
+    # if number is a hexadecimal, strip '0x' and process
+    if num_str[:2] == '0x':
+        num_str = num_str[2:]
+
+        num_str = conv_hex(num_str)
+        if not num_str:
+            return None
+
+        return num_str * negSign
+
+    # count num of periods in string. if 2+ periods rtn None
+    valid_digits = '0987654321.'
+    for i, ch in enumerate(num_str):
+        if ch == '.':
+            perCount += 1
+            charPos = i
+        if perCount > 1 or ch not in valid_digits:
+            return None
+
+    num_str = process_intfloat(num_str, perCount, charPos)
+
+    return num_str * negSign
+
+
+def process_intfloat(num_str, percount, charpos):
+    """strips string of decimals and sends to correct converting function"""
+    # sort thru decimal num placed at start/end of string
+    if percount == 1 and num_str[0] == '.':
+        num_str = conv_frac(num_str[1:])
+    elif percount == 1 and num_str[-1] == '.':
+        num_str = conv_whole(num_str[:-1]) + 0.0
+
+    # split str at decimal point position if nums on both sides of decimal pt
+    elif percount == 1:
+        wholeNum = conv_whole(num_str[:charpos])
+        fracNum = conv_frac(num_str[charpos + 1:])
+        num_str = wholeNum + fracNum + 0.0
+
+    # if string passed is not float, don't convert to float
+    elif percount == 0:
+        num_str = conv_whole(num_str)
+
+    return num_str
+
+
+def conv_whole(num):
+    """converts whole number portion of passed string num. returns an int"""
+    place, converted = 1, 0
+    nums = {'0': 0, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3,
+            '2': 2, '1': 1}
+
+    num = num[::-1]
+    for digit in num:
+        converted += nums[digit] * place
+        place *= 10
+    return converted
+
+
+def conv_frac(num):
+    """converts the fractional portion of string num. returns a float"""
+    place, converted = 10, 0
+    nums = {'0': 0, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3,
+            '2': 2, '1': 1}
+
+    for digit in num:
+        converted += nums[digit] / place
+        place *= 10
+    return converted
+
+
+def conv_hex(num):
+    """converts a valid hex number string. returns an int"""
+    converted = 0
+    hexdict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+               '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14,
+               'f': 15, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+
+    num = num[::-1]
+    for i, value in enumerate(num):
+        if value not in hexdict:
+            return None
+        converted += hexdict[value] * (16 ** i)
+    return converted
+
+
+# Function 2 Author: Ben Depew
+# Main function definition
+def my_datetime(num_sec):
+    num_days = calc_days(num_sec)
+    num_years, num_days = calc_years(num_days)
+    month, day, year = calculate_date(num_years, num_days)
+    date = convert_to_string(month,day,year)
+    return date
+
+
+# Calculate days from seconds
+def calc_days(num_sec):
+    num_days = 0
+    sec_in_day = 86400
+    while num_sec >= sec_in_day:
+        num_sec -= sec_in_day
+        num_days += 1
+    return num_days
+
+
+# Calculate number of 400 year cycles to simplify by
+def calc_years(num_days):
+    days_in_400years = 146097
+    num_years = 0
+
+    while num_days >= 146097:
+        num_days -= 146097
+        num_years += 400
+
+    return num_years, num_days
+
+
+# Calculate month, day, year
+def calculate_date(num_years, num_days):
+    days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    days_in_month_leapyear = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+    current_year = 1970 + num_years
+    current_month = 1
+    current_day = 1
+
+    while num_days:
+        calendar = days_in_month
+        if current_year % 400 == 0:
+            calendar = days_in_month_leapyear
+        elif current_year % 100 == 0:
+            calendar = days_in_month
+        elif current_year % 4 == 0:
+            calendar = days_in_month_leapyear
+        if current_day == calendar[current_month-1]:
+            current_month += 1
+            if current_month == 13:
+                current_month = 1
+                current_year += 1
+            current_day = 0
+        current_day += 1
+        num_days -= 1
+    return current_month, current_day, current_year
+
+
+# Convert month day and year to the right string output
+def convert_to_string(month,day,year):
+    date = str(month) + "-"
+    if month < 10:
+        date = "0" + date
+    if day < 10:
+        date = date + "0"
+    date = date + str(day) + "-" + str(year)
+    return date
+
+#Author: Andrew Yemtsev
+#Function that convert number into a hex letter
+def  lit(x):
+	if x == 15:
+		return "F"
+	elif x == 14:
+		return "E"
+	elif x == 13:
+		return "D"
+	elif x == 12:
+		return "C"
+	elif x == 11:
+		return "B"
+	elif x == 10:
+		return "A"
+	else:
+		return str(x)
+
+# Function 3
+def conv_endian(num, endian='big'):
+	result = ""
+	bytes = []
+	values_num = 0
+	value_counter = 0
+
+	#handle the case when negative integer
+	if num < 0:
+		num *= -1
+		result = "-"
+
+	#check if the amount of characters in hexadecimal number is Odd or Even
+	temp_num = num
+	while temp_num > 0:
+		temp_num = temp_num/16
+		values_num += 1
+
+	if values_num%2 == 0:
+		isOdd = False
+	else:
+		isOdd = True
+
+	#Dec to Hex conversion algorithm
+	bytes.append('')
+	index = 0
+	while num > 0:
+		num, remainder = divmod(num, 16)
+		bytes[index] = lit(remainder) + bytes[index]
+		value_counter += 1
+		if value_counter%2 == 0:
+			index += 1
+			bytes.append('')
+
+	#Handle the case when the amount of charactes in hex number is Odd
+	if isOdd:
+		bytes[index] = '0' + bytes[index]
+
+	#Handle the case when the amount of charactes in hex number is Even
+	else:
+		bytes.pop(len(bytes)-1)
+
+	#Put all bytes in a little endian fashion 
+	if endian == "little":
+		index = 0
+		for x in bytes:
+			index += 1
+			result += x
+			if index != len(bytes):
+				result += ' '
+	
+	#Put all bytes in a big endian fashion 
+	elif endian == "big":
+		index = 0
+		for x in reversed(bytes):
+			index += 1
+			result += x
+			if index != len(bytes):
+				result += ' '
+
+	#Handle the case when the input is neither 'little' nor 'big' for Endian
+	else:
+		result = 'None'
+	
+	return result
--- tests.py
+++ tests.py
@@ -1,11 +1,162 @@
+import random
+import datetime
 import unittest
-
+from task import conv_num
+from task import my_datetime
 
 class TestCase(unittest.TestCase):
-
+    # check if empty argument returns None for function 1
     def test1(self):
-        self.assertTrue(True)
+        num = ''
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 empty str not none={}'.format(num))
 
+    # check var type not str returns None for function 1
+    def test2(self):
+        num = 456
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 non-str type not none={}'.format(num))
+
+    # check hex with incorrect prefix format returns None for function 1
+    def test3(self):
+        num = '0X9B'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 bad hex format not none={}'.format(num))
+
+    # check hex with no numbers after prefix returns None for function 1
+    def test4(self):
+        num = '0x'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 empty hex type not none={}'.format(num))
+
+    # check string with only - sign returns None for function 1
+    def test5(self):
+        num = '-'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 only neg sign not none={}'.format(num))
+
+    # check that neg zero returns zero as float for function 1
+    def test6(self):
+        num = '-0.0'
+        self.assertEqual(conv_num(num), 0.0,
+                         msg='f1 incorrect -0.0 !={}'.format(num))
+
+    # check that neg num with space btw num and sign returns None function 1
+    def test7(self):
+        num = '- 45.7'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 {} not none'.format(num))
+
+    # check that neg num with multi decimal returns None function 1
+    def test8(self):
+        num = '-12.900.88'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 {} multi decimal not none'.format(num))
+
+    # check hex with decimal point returns None function 1
+    def test9(self):
+        num = '0xbd.a'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 hex w/ decimal not none={}'.format(num))
+
+    # check hex with invalid alphabet letter returns None function 1
+    def test10(self):
+        num = '0x0Z0'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 invalid hex letter not None = {}'.format(num))
+
+    # check that float with non-number returns None function 1
+    def test11(self):
+        num = '43.0k5'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 float with non-num not None = {}'.format(num))
+
+    # check valid string int with leading zeros returns int function 1
+    def test12(self):
+        num = '-045'
+        self.assertEqual(conv_num(num), -45,
+                         msg='f1 valid int with lead zeros '
+                             'not valid = {}'.format(num))
+
+    # check successive decimals for number returns None
+    def test13(self):
+        num = '..69420'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 successive decimals not None = '
+                             '{}'.format(num))
+
+    # check decimals at both ends of float returns None
+    def test14(self):
+        num = '.9339.'
+        self.assertEqual(conv_num(num), None,
+                         msg='f1 decimals at both ends not None = '
+                             '{}'.format(num))
+
+    # check consecutive ending decimals returns None
+    def test15(self):
+        num = '-35...'
+        self.assertEqual(conv_num(num), None, msg='f1 trailing decimals '
+                                                  'not None = {}'.format(num))
+
+    # Generate 100 random test cases to verify function 2
+    def test16(self):
+        tests_to_generate = 100
+
+        # Generate random test cases
+        for i in range(tests_to_generate):
+            # Generate test time
+            seconds = random.randint(0,9999999999)
+            output = my_datetime(seconds)
+            date = datetime.datetime.utcfromtimestamp(seconds)
+            expected = date.strftime('%m-%d-%Y')
+            # Generate failure message if my_datetime doesn't match expectation
+            if output != expected:
+                print('Failure: {} should be {}'.format(output, expected))
+
+    #check if decimal 954786 converts to hex number corectly in big endian fashion
+    def test17(self):
+        num = 954786
+        self.assertEqual(conv_endian(num, 'big'), '0E 91 A2')
+
+    #check if the function with no second argument gives the same result as test17
+    def test18(self):
+        num = 954786
+        self.assertEqual(conv_endian(num), '0E 91 A2')
+
+    #check if negative decimal gives the right result
+    def test19(self):
+        num = -954786
+        self.assertEqual(conv_endian(num), '-0E 91 A2')
+
+    #check if little endian works correcly
+    def test20(self):
+        num = 954786
+        self.assertEqual(conv_endian(num, 'little'), 'A2 91 0E')
+
+    #check if negative decimal and little endian work togther correcly 
+    def test21(self):
+        num = -954786
+        self.assertEqual(conv_endian(num, 'little'), '-A2 91 0E')
+
+    #check if 'endian='little' argument works as supposed to 
+    def test22(self):
+        num = -954786
+        self.assertEqual(conv_endian(num, endian='little'), '-A2 91 0E')
+
+    #check if the case when endian argument is incorrect gives the correct result
+    def test22(self):
+        num = -954786
+        self.assertEqual(conv_endian(num, endian='small'), 'None')
+
+    #check if the decimal 4325 gives the correct result
+    def test23(self):
+        num = 4325
+        self.assertEqual(conv_endian(num), '10 E5')
+
+    #check if the decimal -4325 gives the correct result
+    def test24(self):
+        num = 4325
+        self.assertEqual(conv_endian(num, 'little'), 'E5 10')
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
